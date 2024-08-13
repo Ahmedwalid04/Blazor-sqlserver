f37ca09c5b503bd0545b70c6b08473eb5e173ac1
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2024-08-13 09:05:36 +0300

    FOUNDATIONS: add User AutoLogin And Login

--- Blazor-sqlserver/services/Foundation/LoginServices.cs
+++ Blazor-sqlserver/services/Foundation/LoginServices.cs
@@ -1,17 +1,28 @@
-﻿using Dapper;
+﻿using Blazor.Models;
+using Dapper;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Task.Broker.Storages;
 namespace services.Foundation;
-public class LoginServices(StorageBroker storageBroker)
+public class LoginServices(StorageBroker storageBroker, ProtectedLocalStorage LocalStorage)
 {
     public bool IsLoggedIn { get; set; }
-    public async Task<bool> Login(string? username, string? password)
+    public bool Login(string? UserName, string? Password)
     {
-        using (var connection = storageBroker.CreateConnection())
-        {
-            var query = "SELECT COUNT(1) FROM Users WHERE UserName = @UserName AND Password = @Password";
-            var count = await connection.ExecuteScalarAsync<int>(query, new { UserName = username, Password = password });
-            return count > 0;
-        }
+        using var connection = storageBroker.CreateConnection();
+        var query = "SELECT COUNT(1) FROM Users WHERE UserName = @UserName AND Password = @Password";
+        var count = connection.ExecuteScalar<int>(query, new { UserName , Password  });
+        return count > 0;
+
+    }
+    public void Login(Users Users)
+     => IsLoggedIn = storageBroker.SelectUserByUsernameAndPassword(
+         Users.UserName ?? string.Empty,
+         Users.Password ?? string.Empty) is not null;
+
+    public async ValueTask AutoLogin()
+    {
+        var x = await LocalStorage.GetAsync<bool?>("IsLoggedIn");
+        IsLoggedIn = x.Value ?? false;
     }
 }
 
