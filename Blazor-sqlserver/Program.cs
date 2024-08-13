f97654b2fcc44bac63e29d937ae67a55b4eb0bc7
Ahmed Walid <119056772+Ahmedwalid04@users.noreply.github.com>
2024-08-13 07:38:25 +0300

    EXPOSERS

--- Blazor-sqlserver/Program.cs
+++ Blazor-sqlserver/Program.cs
@@ -1,15 +1,12 @@
 using Task.Broker.Storages;
 using Blazor_sqlserver.Components;
-using Blazor.Services;
 using Arora.Blazor.StateContainer;
 using services.Foundation;
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton(new StorageBroker(builder.Configuration));
 builder.Services.AddTransient<StorageBroker>();
 builder.Services.AddScoped<ProductServices>()
                 .AddScoped<LoginServices>()
-                .AddScoped<UserService>()
                 .AddStateContainer()
                 .AddRazorComponents()
     .AddInteractiveServerComponents();
